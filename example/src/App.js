a2c60828093255aaf8e6db86993828cf8d9a6006
neuroidsolutions <neuroidsolutions@gmail.com>
2020-04-08 22:42:16 +0530

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-neutable'
+import ReactNeuTable from "react-neutable";
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ReactNeuTable
+          heading={[
+            { name: "#", type: "number", key: "id" },
+            { name: "First", type: "string", key: "fName" },
+            { name: "Last", type: "string", key: "lName" },
+            { name: "Name", type: "string", key: "name" }
+          ]}
+          rows={[
+            { id: 1, fName: "Midhun", lName: "E M", name: "Midhun Em" },
+            { id: 2, fName: "Arjun", lName: "xm", name: "Arjun Em" }
+          ]}
+        />
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,117 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+class ReactNeuTable extends Component {
   static propTypes = {
-    text: PropTypes.string
+    heading: PropTypes.arrayOf(
+      PropTypes.exact({
+        name: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['number', 'string']).isRequired,
+        key: PropTypes.string.isRequired
+      })
+    ).isRequired,
+    tableName: PropTypes.string.isRequired,
+    rows: PropTypes.array.isRequired
+  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeItem: '',
+      sort: true,
+      active: true,
+      items: []
+    }
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  sort = tableHead => {
+    const { activeItem } = this.state
+    const sort = activeItem === tableHead.name ? !this.state.sort : true
+    let sortedItems = []
+    if (tableHead.type === 'string') {
+      const property = sort ? tableHead.key : `-${tableHead.key}`
+      sortedItems = this.props.rows.sort(this.dynamicSort(property))
+    } else {
+      const items = this.props.rows.sort((a, b) => a - b)
+      const beforeSort = [...items]
+      sortedItems = sort ? beforeSort : beforeSort.reverse()
+    }
 
+    this.setState({
+      sort: sort,
+      activeItem: tableHead.name,
+      items: sortedItems
+    })
+  };
+
+  dynamicSort = property => {
+    let sortOrder = 1
+    if (property[0] === '-') {
+      sortOrder = -1
+      property = property.substr(1)
+    }
+    return function(a, b) {
+      if (sortOrder === -1) {
+        return b[property].localeCompare(a[property])
+      } else {
+        return a[property].localeCompare(b[property])
+      }
+    }
+  };
+
+  render() {
+    const { heading, tableName, rows } = this.props
+    const { sort, activeItem, items } = this.state
+    const ascSort = [styles.i, styles.down].join(' ')
+    const descSort = [styles.i, styles.up].join(' ')
+    const activeClass = sort === true ? ascSort : descSort
+    const rowElements = activeItem === '' ? rows : items
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {heading.map((tableHead, index) => {
+              return (
+                <th
+                  key={`ReactNeuTable_tableHead_${tableName}_${index}`}
+                  className={styles.theadTh}
+                  onClick={() => {
+                    this.sort(tableHead)
+                  }}
+                >
+                  <span>{tableHead.name}</span>
+                  <i
+                    className={activeClass}
+                    style={{
+                      visibility: activeItem === tableHead.name ? 'visible' : null
+                    }}
+                  />
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rowElements.map((rowElement, index) => {
+            return (
+              <tr key={`ReactNeuTable_tr_${tableName}_${index}`}>
+                {heading.map((headItem, rowKeyIndex) => {
+                  return (
+                    <td
+                      key={`ReactNeuTable_rowKeys_${tableName}_${rowKeyIndex}`}
+                    >
+                      {rowElement[headItem.key]}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     )
   }
 }
+ReactNeuTable.defaultProps = {
+  tableName: `ReactNeuTable_${Math.random() * 100}`
+}
+export default ReactNeuTable
